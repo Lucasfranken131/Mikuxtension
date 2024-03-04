cee028da41147daf93bdaef9bc66e8f1f8888c22
Lucasfranken131 <lucas.franco@aluno.fmpsc.edu.br>
2024-03-04 17:17:58 -0300

    Miku na Web

--- mikuPage.js
+++ mikuPage.js
@@ -1,21 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('walk').addEventListener('click', walk);
-    var mikuImg;
-    alert("funcionando")
-    function walk() {
+    var walkButton = document.getElementById('walk');
+    walkButton.addEventListener('click', function() {
+        var mikuImg;
         if(!mikuImg) {
             mikuImg = document.createElement('img');
-            mikuImg.src = './imgs/miku_character/miku_walking_right.gif';
+            mikuImg.src = chrome.runtime.getURL('./imgs/miku_character/miku_walking_right.gif');
             mikuImg.style.position = 'fixed';
             mikuImg.style.top = '50px'; // Posição superior da imagem
             mikuImg.style.left = '50px'; // Posição esquerda da imagem
             mikuImg.style.zIndex = '9999'; // Para garantir que a imagem fique acima de outros elementos na página
             document.body.appendChild(mikuImg);
             // Adiciona evento de clique na imagem
-    
+        
             mikuImg.addEventListener('click', function() {
                 alert('Você clicou na imagem da Miku!');
             });
         }
-    }
+    });
 });
+
+
--- popUp.js
+++ popUp.js
@@ -1,8 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('walk').addEventListener('click', function () {
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) { 
-            var tabURL = tabs[0].url;
-            console.log(tabURL);
-        });
-    });
-});
+// function getWalk() {
+//     document.getElementById('walk').addEventListener('click', () => {
+//         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//             const tab = tabs[0];
+//             chrome.scripting.executeScript({
+//                 target: { tabId: tab.id },
+//                 files: ['mikuPage.js']
+//                 //        files: ['contentScript.js'],  // To call external file instead
+//             }).then(() => console.log('Injected a function!'));
+//         });
+//     });
+// }
+
+window.onload = getWalk;
