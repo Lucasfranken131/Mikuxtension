798c2a6dc1f79f6abcd6fdf4dad88b5df84b33d4
Lucasfranken131 <lucas.franco@aluno.fmpsc.edu.br>
2024-03-04 20:55:55 -0300

    felicidade

--- mikuPage.js
+++ mikuPage.js
@@ -1,22 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var walkButton = document.getElementById('walk');
-    walkButton.addEventListener('click', function() {
-        var mikuImg;
-        if(!mikuImg) {
-            mikuImg = document.createElement('img');
-            mikuImg.src = chrome.runtime.getURL('./imgs/miku_character/miku_walking_right.gif');
-            mikuImg.style.position = 'fixed';
-            mikuImg.style.top = '50px'; // Posição superior da imagem
-            mikuImg.style.left = '50px'; // Posição esquerda da imagem
-            mikuImg.style.zIndex = '9999'; // Para garantir que a imagem fique acima de outros elementos na página
-            document.body.appendChild(mikuImg);
-            // Adiciona evento de clique na imagem
-        
-            mikuImg.addEventListener('click', function() {
-                alert('Você clicou na imagem da Miku!');
-            });
-        }
-    });
-});
+var mikuImg;
+if(!mikuImg) {
+    mikuImg = document.createElement('img');
+    mikuImg.src = chrome.runtime.getURL('./imgs/miku_character/miku_walking_right.gif');
+    mikuImg.style.position = 'fixed';
+    mikuImg.style.top = '50px';
+    mikuImg.style.left = '50px';
+    mikuImg.style.zIndex = '9999';
+    document.body.appendChild(mikuImg);
 
+    mikuImg.addEventListener('click', function() {
+        alert('Você clicou na imagem da Miku!');
+    });
+}
 
--- popUp.js
+++ popUp.js
@@ -1,14 +1,14 @@
-// function getWalk() {
-//     document.getElementById('walk').addEventListener('click', () => {
-//         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//             const tab = tabs[0];
-//             chrome.scripting.executeScript({
-//                 target: { tabId: tab.id },
-//                 files: ['mikuPage.js']
-//                 //        files: ['contentScript.js'],  // To call external file instead
-//             }).then(() => console.log('Injected a function!'));
-//         });
-//     });
-// }
+function getWalk() {
+    document.getElementById('walk').addEventListener('click', () => {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            const tab = tabs[0];
+            chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                files: ['mikuPage.js']
+                //        files: ['contentScript.js'],  // To call external file instead
+            }).then(() => console.log('Injected a function!'));
+        });
+    });
+}
 
 window.onload = getWalk;
