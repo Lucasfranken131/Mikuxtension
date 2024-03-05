5cb56288d772862a224f7a93ddef342fe00d97fe
Lucasfranken131 <lucas.franco@aluno.fmpsc.edu.br>
2024-03-05 15:12:47 -0300

    ela está se movendo

--- mikuPage.js
+++ mikuPage.js
@@ -1,15 +1,35 @@
 var mikuImg;
-if(!mikuImg) {
+var y = 200;
+var x = 200;
+function createMiku() {
     mikuImg = document.createElement('img');
     mikuImg.src = chrome.runtime.getURL('./imgs/miku_character/miku_walking_right.gif');
-    mikuImg.style.position = 'fixed';
-    mikuImg.style.top = '50px';
-    mikuImg.style.left = '50px';
-    mikuImg.style.zIndex = '9999';
+    mikuImg.style.position = 'absolute';
+
     document.body.appendChild(mikuImg);
 
     mikuImg.addEventListener('click', function() {
         alert('Você clicou na imagem da Miku!');
     });
+    setInterval(moveMiku, 10);
+
+    function moveMiku() {
+        if(x < window.screen.width - mikuImg.width) {
+            x += 5
+        }
+        if(y < window.screen.height - mikuImg.height)  {
+            y += 0
+        }
+        mikuImg.style.top = y + 'px';
+        mikuImg.style.left = x + 'px';
+    }
 }
 
+if(!mikuImg) {
+    createMiku();
+}
+
+else {
+    document.body.removeChild(mikuImg);
+    mikuImg = null;
+}
--- popUp.js
+++ popUp.js
@@ -1,14 +1,14 @@
-function getWalk() {
-    document.getElementById('walk').addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('walk').addEventListener('click', function() {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            console.log("bosta")
             const tab = tabs[0];
             chrome.scripting.executeScript({
                 target: { tabId: tab.id },
                 files: ['mikuPage.js']
-                //        files: ['contentScript.js'],  // To call external file instead
             }).then(() => console.log('Injected a function!'));
         });
     });
-}
+});
 
-window.onload = getWalk;
+
