9aa0a5031bf03414b0a518199d955f8433e1aeb9
Lucasfranken131 <lucas.franco@aluno.fmpsc.edu.br>
2024-03-05 17:08:49 -0300

    Ela possui inteligência

--- mikuPage.js
+++ mikuPage.js
@@ -1,6 +1,7 @@
-var mikuImg;
-var y = 200;
-var x = 200;
+var mikuImg = null;
+var y = 500;
+var x = 1000;
+var direction = Math.floor(Math.random() * 4);
 function createMiku() {
     mikuImg = document.createElement('img');
     mikuImg.src = chrome.runtime.getURL('./imgs/miku_character/miku_walking_right.gif');
@@ -9,27 +10,55 @@ function createMiku() {
     document.body.appendChild(mikuImg);
 
     mikuImg.addEventListener('click', function() {
-        alert('Você clicou na imagem da Miku!');
+        alert("Miku foi eliminada.");
+        document.body.removeChild(mikuImg);
+        mikuImg = null;
     });
     setInterval(moveMiku, 10);
+    setInterval(changeDirection, 1000);
 
     function moveMiku() {
-        if(x < window.screen.width - mikuImg.width) {
-            x += 5
+        if (x < window.screen.width - mikuImg.width && y < window.screen.height - mikuImg.height) {
+            mikuDirection();
         }
-        if(y < window.screen.height - mikuImg.height)  {
-            y += 0
+        else {
+            x = 500
+            y = 500
         }
+
+        x = Math.max(x, 0);
+        y = Math.max(y, 0);
+
         mikuImg.style.top = y + 'px';
         mikuImg.style.left = x + 'px';
+    }
+
+    function changeDirection() {
+        direction = Math.floor(Math.random() * 4);
+        if(direction == 1) {
+            mikuImg.src = chrome.runtime.getURL('./imgs/miku_character/miku_walking_left.gif');
+        }
+        if(direction == 3) {
+            mikuImg.src = chrome.runtime.getURL('./imgs/miku_character/miku_walking_right.gif');
+        }
+    }
+
+    function mikuDirection() {
+        if(direction == 0) {
+            y -= 5
+        }
+        if(direction == 1) {
+            x -= 5
+        }
+        if(direction == 2) {
+            y += 5
+        }
+        if(direction == 3) {
+            x += 5
+        }
     }
 }
 
 if(!mikuImg) {
     createMiku();
-}
-
-else {
-    document.body.removeChild(mikuImg);
-    mikuImg = null;
 }
--- popUp.js
+++ popUp.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('walk').addEventListener('click', function() {
+    document.getElementById('walk').addEventListener('click', () => {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            console.log("bosta")
             const tab = tabs[0];
             chrome.scripting.executeScript({
                 target: { tabId: tab.id },
